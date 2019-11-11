e207a7397cd6fb3e5c8b7203758aaf76bfe0b879
letstrie <2016-314-436@student.cse.du.ac.bd>
2019-11-12 00:41:32 +0600

    create new disucssion

--- server/Forum/Forum.controller.js
+++ server/Forum/Forum.controller.js
@@ -1,3 +1,11 @@
 module.exports.getForum = async (req, res) => {
   return res.render('forum');
 };
+
+module.exports.getSingleDiscussion = async (req, res) => {
+  return res.render('singleDiscussion');
+};
+
+module.exports.getNewDiscussion = async (req, res) => {
+  return res.render('createDiscussion');
+};
--- server/Forum/Forum.route.js
+++ server/Forum/Forum.route.js
@@ -4,5 +4,7 @@ const controller = require('./Forum.controller');
 const router = express.Router();
 
 router.get('/', controller.getForum);
+router.get('/new', controller.getNewDiscussion);
+router.get('/single', controller.getSingleDiscussion);
 
 module.exports = router;

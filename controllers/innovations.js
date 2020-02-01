1dcef5acd0eb6535c2b6bd2fc7ef0f08d23fba1d
dujoker70 <merajularefin@gmail.com>
2020-02-01 16:48:20 +0600

    Innovations shown in innovation route

--- controllers/innovations.js
+++ controllers/innovations.js
@@ -1,11 +1,11 @@
 const innovationModel = require('../models/innovations');
 
-module.exports.innovations = (req, res, next) => {
-    innovationModel.find({}, (err, result) => {
-        if (err) throw err;
-        console.log(result);
+module.exports.innovations = async (req, res, next) => {
+    await innovationModel.find({}, (err, result) => {
+        return res.render('innovations', {
+            data: result
+        });
     });
-    return res.render('innovations');
 };
 
 module.exports.createInnovations = (req, res, next) => {
@@ -13,11 +13,6 @@ module.exports.createInnovations = (req, res, next) => {
 };
 
 module.exports.postInnovations = async (req, res, next) => {
-    if (typeof req.body.tag != 'string') {
-        console.log(req.body);
-        res.send(typeof req.body.tags);
-        return;
-    }
     const data = new innovationModel({
         'title': req.body.title,
         'pageType': req.body.pagetype,
@@ -25,7 +20,10 @@ module.exports.postInnovations = async (req, res, next) => {
         'objective': req.body.summary,
         'content': req.body.content
     });
-    console.log(data);
     await data.save();
-    return res.render('innovations');
+    await innovationModel.find({}, (err, result) => {
+        return res.render('innovations', {
+            data: result
+        });
+    });
 };

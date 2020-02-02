474dda249855498e4e753264ec37b8ba162c837a
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-02-02 12:56:23 +0600

    Doctor Profile Created

--- routes/services.js
+++ routes/services.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 router.get('/consultation', (req, res, next) => res.render('consultation'));
-
+router.get('/ourDoctors', (req, res, next) => res.render('ourDoctors'));
+router.get('/doctor/profile/:id', (req, res, next) => {
+  return res.render('doctorsProfile');
+});
 module.exports = router;

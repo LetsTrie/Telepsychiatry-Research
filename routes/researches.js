23e1cbca64fff8ffa132294de06dd401a294c96b
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-02-10 01:25:27 +0600

    Some New Page Created

--- routes/researches.js
+++ routes/researches.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const resources = require('../controllers/resources')
+const resources = require('../controllers/resources');
 
-router.get("/",
-    resources.get_resources
-);
+router.get('/', resources.get_resources);
 router.get('/new', (req, res, next) => res.render('createResearches'));
-router.post('/new_research', resources.post_resources)
+router.post('/new_research', resources.post_resources);
+router.get('/single', (req, res, next) => res.render('singleResearch'));
 
 module.exports = router;

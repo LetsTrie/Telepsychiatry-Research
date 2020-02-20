cf5f4545831e4d7f29bd1d229dec8f51d863be62
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-02-20 12:43:46 +0600

    Working on Skill Development

--- controllers/events.js
+++ controllers/events.js
@@ -1,30 +1,33 @@
-const { eventsModel } = require('../models/events')
+// const { eventsModel } = require('../models/events')
 
-exports.getEvents = async(req, res, next) => {
-    const events = await eventsModel.find()
-    res.render('events', {
-        posts: events
-    })
-}
+// exports.getEvents = async(req, res, next) => {
+//     const events = await eventsModel.find()
+//     res.render('events', {
+//         posts: events
+//     })
+// }
 
-exports.createEvent = async(req, res, next) => {
-    const {
-        title,
-        description,
-        location,
-        date,
-        time
-    } = req.body
+// exports.createEvent = async(req, res, next) => {
+//     const {
+//         title,
+//         description,
+//         location,
+//         date,
+//         time
+//     } = req.body
 
-    const newEvent = await new eventsModel({
-        title,
-        description,
-        location,
-        date,
-        time
-    }).save()
+//     const newEvent = await new eventsModel({
+//         title,
+//         description,
+//         location,
+//         date,
+//         time
+//     }).save()
 
-    console.log(newEvent)
-    req.flash('created', 'event created')
-    res.redirect('/events')
-}
+//     console.log(newEvent)
+//     req.flash('created', 'event created')
+//     res.redirect('/events')
+// }
+
+exports.getWorkshop = (req, res, next) => res.render('listOfWorkshop');
+exports.getTraining = (req, res, next) => res.render('listOfTrainingSession');
--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const { getEvents, createEvent } = require('../controllers/events')
+const { getWorkshop, getTraining } = require('../controllers/events');
 
-router.get('/', getEvents);
-router.get('/new', (req, res, next) => res.render('createEvents'));
-router.get('/single', (req, res, next) => res.render('singleEvent'));
-router.post('/new', createEvent)
+// router.get('/', getEvents);
+// router.get('/new', (req, res, next) => res.render('createEvents'));
+// router.get('/single', (req, res, next) => res.render('singleEvent'));
+// router.post('/new', createEvent)
+
+router.get('/workshop', getWorkshop);
+router.get('/training', getTraining);
 
 module.exports = router;

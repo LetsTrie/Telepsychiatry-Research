f0394e96b89cafb86407edc63c8b77edc75a8439
Hussain-Safwan <safwan.du16@gmail.com>
2020-03-27 10:19:53 +0600

    institution page added

--- routes/services.js
+++ routes/services.js
@@ -4,6 +4,9 @@ router.get('/consultation', (req, res, next) => res.render('consultation'));
 router.get('/psychoTherapy', (req, res, next) => res.render('psycho_therapy'));
 router.get('/ourDoctors', (req, res, next) => res.render('ourDoctors'));
 router.get('/doctor/profile/:id', (req, res, next) => {
-  return res.render('doctorsProfile');
+    return res.render('doctorsProfile');
+});
+router.get('/institution', (req, res, next) => {
+    res.render('institution');
 });
 module.exports = router;

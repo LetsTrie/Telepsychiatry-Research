53796c40dea442b63545a22ce85cb92b677e5310
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-03-28 17:00:14 +0600

    assessment done

--- routes/services.js
+++ routes/services.js
@@ -4,9 +4,21 @@ router.get('/consultation', (req, res, next) => res.render('consultation'));
 router.get('/psychoTherapy', (req, res, next) => res.render('psycho_therapy'));
 router.get('/ourDoctors', (req, res, next) => res.render('ourDoctors'));
 router.get('/doctor/profile/:id', (req, res, next) => {
-    return res.render('doctorsProfile');
+  return res.render('doctorsProfile');
 });
 router.get('/institution', (req, res, next) => {
-    res.render('institution');
+  res.render('institution');
+});
+
+router.get('/assessment', (req, res, next) => {
+  res.render('assessment');
+});
+
+router.get('/assessment/questions', (req, res, next) => {
+  res.render('assessment_ques');
+});
+
+router.get('/assessment/result', (req, res, next) => {
+  res.render('assessment_result');
 });
 module.exports = router;

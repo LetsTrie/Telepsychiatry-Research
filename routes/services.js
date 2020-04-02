0dbac609724b752dbc03690ff85de2ddf17279e9
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-04-02 22:05:28 +0600

    doctor page redesigned

--- routes/services.js
+++ routes/services.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 
 router.get('/consultation', (req, res, next) => res.render('consultation'));
 router.get('/psychoTherapy', (req, res, next) => res.render('psycho_therapy'));
-router.get('/ourDoctors', (req, res, next) => res.render('ourDoctors'));
 router.get('/doctor/profile/:id', (req, res, next) => {
   return res.render('doctorsProfile');
 });
@@ -23,6 +22,10 @@ router.get('/assessment/quiz', (req, res, next) => {
 
 router.get('/assessment/register', (req, res, next) => {
   res.render('assessment_reg');
+});
+
+router.get('/special_services', (req, res, next) => {
+  res.render('specialServices');
 });
 
 module.exports = router;

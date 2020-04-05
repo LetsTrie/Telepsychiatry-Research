b0ea6d88668042c117bdc686568f51d725f02af0
Hussain-Safwan <safwan.du16@gmail.com>
2020-04-05 11:38:16 +0600

    previous changes merged

--- app.js
+++ app.js
@@ -15,14 +15,13 @@ const app = express();
 app.set('view engine', 'ejs');
 
 mongoose.connect(
-  process.env.mongoURI,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  },
-  () => console.log('connected to database!')
+    process.env.mongoURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    },
+    () => console.log('connected to database!')
 );
 
 app.use(express.static('client'));
@@ -31,49 +30,51 @@ app.use(express.static('public'));
 
 const multer = require('multer');
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  },
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads/');
+    },
+    filename: (req, file, cb) => {
+        const filename = req.body.filename + '-' + file.originalname;
+        cb(null, filename);
+    }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'application/pdf'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 
 app.use(
-  multer({
-    storage: fileStorage,
-    fileFilter: fileFilter,
-  }).single('image')
+    multer({
+        storage: fileStorage,
+        fileFilter: fileFilter,
+    }).fields([{ name: 'exp_user_propic' }, { name: 'exp_cv' }])
 );
 
 if (process.env.NODE_ENV === 'development') app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-  })
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+    })
 );
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.successMessage = req.flash('successMessage');
-  res.locals.errorMessage = req.flash('errorMessage');
-  res.locals.alertMessage = req.flash('alertMessage');
-  next();
+    res.locals.successMessage = req.flash('successMessage');
+    res.locals.errorMessage = req.flash('errorMessage');
+    res.locals.alertMessage = req.flash('alertMessage');
+    next();
 });
 app.use(compress());
 app.use(helmet());
--- validations/auth.js
+++ validations/auth.js
@@ -1,42 +1,129 @@
 const joi = require('@hapi/joi');
 
 exports.regGenUserVal = data => {
-  const compareWith = joi.object({
-    fname: joi
-      .string()
-      .required()
-      .regex(/^[a-z A-Z](?!.* {2})[ \w.-]{2,24}$/),
-    lname: joi
-      .string()
-      .required()
-      .regex(/^[a-z A-Z](?!.* {2})[ \w.-]{2,24}$/),
-    email: joi
-      .string()
-      .required()
-      .regex(
-        /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-      ),
-    password: joi
-      .string()
-      .required()
-      .min(6),
-    cPassword: joi
-      .string()
-      .required()
-      .min(6),
-    phoneNumber: joi
-      .string()
-      .required()
-      .regex(/\w+/i)
-      .min(7),
-    gender: joi.string().required(),
-    country: joi.string().required(),
-    dob: joi
-      .string()
-      .required()
-      .regex(/^(\d{2})\/(\d{2})\/(\d{4})$/),
-    cAffiliation: joi.string().required(),
-    hADegree: joi.string().required()
-  });
-  return compareWith.validate(data);
+    const compareWith = joi.object({
+        fname: joi
+            .string()
+            .required()
+            .regex(/^[a-z A-Z](?!.* {2})[ \w.-]{2,24}$/),
+        lname: joi
+            .string()
+            .required()
+            .regex(/^[a-z A-Z](?!.* {2})[ \w.-]{2,24}$/),
+        email: joi
+            .string()
+            .required()
+            .regex(
+                /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+            ),
+        password: joi
+            .string()
+            .required()
+            .min(6),
+        cPassword: joi
+            .string()
+            .required()
+            .min(6),
+        phoneNumber: joi
+            .string()
+            .required()
+            .regex(/\w+/i)
+            .min(7),
+        gender: joi.string().required(),
+        country: joi.string().required(),
+        dob: joi
+            .string()
+            .required()
+            .regex(/^(\d{2})\/(\d{2})\/(\d{4})$/),
+        cAffiliation: joi.string().required(),
+        hADegree: joi.string().required()
+    });
+    return compareWith.validate(data);
+};
+
+exports.regExpUserVal = data => {
+    const compareWith = joi.object({
+        name: joi.string().required(),
+        email: joi
+            .string()
+            .regex(
+                /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+            )
+            .required(),
+        gender: joi.string().allow('', null),
+        dob: joi.string().allow('', null),
+        speciality: joi.string().allow('', null),
+        propicURL: joi.string().required(),
+        cvURL: joi.string().required(),
+        institute: joi.string().allow('', null),
+        expertise: joi.string().allow('', null),
+        phone: joi
+            .string()
+            .allow('', null)
+            .min(6)
+            .regex(/\w+/i),
+        affiliation: joi.string().required(),
+        researchArea: joi.string().allow('', null),
+        professionalDegree: joi.string().allow('', null),
+        regno: joi.string().allow('', null),
+        country: joi.string().allow('', null),
+        fee: joi
+            .string()
+            .regex(/\w+/i)
+            .allow('', null),
+        aboutYourself: joi.string().allow('', null),
+        designation: joi.string().required(),
+        password: joi
+            .string()
+            .required()
+            .min(6),
+        cpassword: joi
+            .string()
+            .allow('', null)
+            .min(6),
+        education: joi.array().items(
+            joi.object({
+                eduInstitute: joi.string().required(),
+                degree: joi.string().required(),
+                eduFrom: joi.string().allow('', null),
+                eduTo: joi.string().allow('', null)
+            })
+        ),
+        training: joi.array().items(
+            joi.object({
+                trainingName: joi.string().allow('', null),
+                trainingYear: joi.string().allow('', null),
+                trainingDetails: joi.string().allow('', null)
+            })
+        ),
+        awards: joi.array().items(
+            joi.object({
+                awardsName: joi.string().allow('', null),
+                awardsYear: joi.string().allow('', null),
+                awardsDetails: joi.string().allow('', null)
+            })
+        ),
+        workExperience: joi.array().items(
+            joi.object({
+                expInstitute: joi.string().allow('', null),
+                expFrom: joi.string().allow('', null),
+                expTo: joi.string().allow('', null)
+            })
+        ),
+        visitingHour: joi.array().items(
+            joi.object({
+                chamberName: joi.string().allow('', null),
+                chamberAddress: joi.string().allow('', null),
+                chamberTimings: joi.array().items(
+                    joi.object({
+                        dayFrom: joi.string().allow('', null),
+                        dayTo: joi.string().allow('', null),
+                        timeFrom: joi.string().allow('', null),
+                        timeTo: joi.string().allow('', null)
+                    })
+                )
+            })
+        )
+    });
+    return compareWith.validate(data);
 };

428ac782a35a2fff1b65ec439f068ae20243c8f3
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-05-01 12:15:30 +0600

    Homepage content changes

---
+++ data/homepage_experts.js
@@ -0,0 +1,48 @@
+module.exports = [
+  {
+    _id: '5ea6a986266de7450f67a0f5',
+    name: 'Tanjir Rashid Soron',
+    bio:
+      'We provide the corporate health Care for various organizations Next-generation wellness service for employer and employee. We treat few but energise all.',
+    designation: 'Founder and Managing Director ',
+    url: '1',
+  },
+  {
+    _id: '5e998ecb4fda502e5399178c',
+    name: 'Ashik Rahman',
+    bio:
+      'I am an enthusiastic researcher-practitioner in the field of clinical psychology and I love to divide my time between clinical practice, training, consultation, and research. In my clinical practice, I deliver cognitive behavioral therapy (CBT) and other evidence-based treatments to adult and adolescent clients with a wide range of emotional, behavioral, adjustment, and chronic health related problems such as anxiety, stress, depression, trauma, chronic pain, drug addiction, and relationship problems. I am also a certified ADOS-2 administrator and I have conducted more than 500 ADOS-2 tests. In my consulting work, I have provided intensive supervision to trainee clinical psychologists as they acquired skills to deliver CBT in their own practice. I am passionate about changing the mental health service scenario in Bangladesh by conveying evidence based services to people and generating a positive impact on their life through quality care. I am eager to apply my experience and compassion with clients as they begin to address issues that will help them to improve their quality of life and attain a sense of fulfillment. I have always been involved with extra-curricular activities since my college days. I have been a volunteer and organizing member of different national and international conferences organized by Department of Clinical Psychology, University of Dhaka. I have also facilitated different workshops and seminars on different mental health related issues such as stress management, anger management, assertiveness training, parenting training, chronic pain management, psychological first aid, mindfulness etc.     I have received my B.S. with honors in psychology, M.S. in clinical psychology, and M.Phil in Clinical Psychology from University of Dhaka. I have completed my clinical psychology internship from National Institute of Mental Health and Hospital and Dhaka Medical College and Hospital. Currently I am a general member of Bangladesh Clinical Psychology Society. I can be contacted at ashiquir1001rahaman@gmail.com',
+    designation: 'Clinical Psychologist',
+    url: '2',
+  },
+  {
+    _id: '5eaa7cfe266de7450f67a0ff',
+    name: 'Md Ashiqur Rahman Ashiq',
+    bio:
+      'A young physician and public health professional from Bangladesh. Interested in public health researches and global mental health.',
+    designation: 'Clinical Research Associate',
+    url: '3',
+  },
+  {
+    _id: '5e9991964fda502e5399178e',
+    name: 'Marzia Al-Hakeem',
+    bio: `I envision being a Clinical Psychologist and a dedicated agent in the field of mental health and therefore I am ready to seek every opportunity to learn, practice and take up challenges as much as I can to develop myself and to contribute to the betterment of the society.`,
+    designation: 'Clinical Psychologist',
+    url: '4',
+  },
+  {
+    _id: '5e9fe83b2793470405c3d8ff',
+    name: 'Saurav Baidya',
+    bio: `A promising young psychologist with a vast experience over Telepsychology, CBT technique, Supervision, Online Counseling, Corporate Counseling and many more fields in psychology. He has a strong hand over the newest technologies and working on how to imply them for the betterment of mental health. He has the capability to motivate anyone and make them feel alive within a moment he attached with them. That’s why he is doing significantly fantastic in the field of mental health. During his short 5 working years, he done some significant works for his serving organizations. He has great command over counseling techniques and having an extrovert approach, he can discover the excellence of any human mind. Alongside he has outstanding knowledge on CBT techniques & Transactional Analysis. He is an expert to conduct any mental health related workshops and seminars. He has conducted 70+ workshops during his career. Most of his workshops are focused on mental health like Anxiety, Depression, Psychological First Aid, Supervision, Stress Management, Anger Management, Self Esteem, Problem Solving, Post Traumatic Stress Disorder and so on. He is an extraordinary communicator who believes, "The most important thing in communication is hearing what isn't said" which prolepsis himself for being a good observer of the non-verbs. In addition, he has a vast and sole experience in tele-counselling where he dealt with assessment, diagnosis and treatment, emotional support for various clients having mental health issues. He also has the experience of working in corporate, school, college, university, clinic, refugee, in-house, organizational & online settings where he had to conduct individual, group, couple & family counseling to develop productive and functional lifestyle.`,
+    designation: 'Psychologist',
+    url: '5',
+  },
+  {
+    _id: '5e9ff70a2793470405c3d913',
+    name: 'Sharmin Ara',
+    bio:
+      'I am an M. Phil researcher (part-2) at the Dept. of Clinical Psychology, University of Dhaka. I work as a psychologist, researcher, trainer and clinical supervisor in mental health fields. I am a Cognitive Behavior Therapy (CBT) practitioner and a trainee in Systemic Family Therapy (SFT) as well.',
+    designation: 'Clinical Psychologist',
+    url: '6',
+  },
+];

4396ab97e07ff3597644e3ba7b4e7134d726d43c
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-05-04 12:28:16 +0600

    Resource Update

--- controllers/innovations.js
+++ controllers/innovations.js
@@ -5,101 +5,117 @@ const LIMIT = 9;
 
 const { makeSmallParagraphFromHTML } = require('./utils');
 
-exports.getInnovation = async(req, res) => {
-    const data = await InnovationModel.findById(req.params.id);
-    res.render('singleInnovations', { data, user: req.user });
+exports.getInnovation = async (req, res) => {
+  const data = await InnovationModel.findById(req.params.id);
+  res.render('singleInnovations', { data, user: req.user });
 };
 
-exports.getInnovations = async(req, res) => {
-    let { stage, searchInput, type } = req.query;
-    let data;
-    // console.log(searchInput);
-    if (searchInput) {
-        const keyword = {
-            innovationStage: type,
-            isVerified: true,
-        };
-        searchInput = searchInput.trim();
-        let searchOptions = {
-            $regex: searchInput,
-            $options: 'i',
-        };
-        let searchKey = {
-            $or: [
-                { title: searchOptions },
-                { description: searchOptions },
-                { name: searchOptions },
-                { designation: searchOptions },
-                { email: searchOptions },
-                { phone: searchOptions },
-                { collaboration: searchOptions },
-                { collabScope: searchOptions },
-                { newsAndPub: searchOptions },
-            ],
-        };
-        data = await InnovationModel.find({
-            $and: [keyword, searchKey],
-        });
-        console.log(type);
-        if (type == 'ongoing') {
-            res.render('ongoingInnovations', { data, search: null });
-        } else {
-            res.render('completeInnovations', { data, search: null });
-        }
-    }
+exports.getInnovations = async (req, res) => {
+  let { stage, searchInput, type } = req.query;
+  let data;
+  // console.log(searchInput);
+  if (searchInput) {
+    const keyword = {
+      innovationStage: type,
+      isVerified: true,
+    };
+    searchInput = searchInput.trim();
+    let searchOptions = {
+      $regex: searchInput,
+      $options: 'i',
+    };
+    let searchKey = {
+      $or: [
+        { title: searchOptions },
+        { description: searchOptions },
+        { name: searchOptions },
+        { designation: searchOptions },
+        { email: searchOptions },
+        { phone: searchOptions },
+        { collaboration: searchOptions },
+        { collabScope: searchOptions },
+        { newsAndPub: searchOptions },
+      ],
+    };
     data = await InnovationModel.find({
-        innovationStage: stage,
-        isVerified: true,
+      $and: [keyword, searchKey],
     });
-    if (stage == 'ongoing') {
-        res.render('ongoingInnovations', { data, search: null });
+    console.log(type);
+    if (type == 'ongoing') {
+      res.render('ongoingInnovations', {
+        data,
+        search: null,
+        user: req.user,
+      });
     } else {
-        res.render('completeInnovations', { data, search: null });
+      res.render('completeInnovations', {
+        data,
+        search: null,
+        user: req.user,
+      });
     }
+  }
+  data = await InnovationModel.find({
+    innovationStage: stage,
+    isVerified: true,
+  });
+  if (stage == 'ongoing') {
+    res.render('ongoingInnovations', {
+      data,
+      search: null,
+      user: req.user,
+    });
+  } else {
+    res.render('completeInnovations', {
+      data,
+      search: null,
+      user: req.user,
+    });
+  }
 };
 
 exports.getNewInnovations = (req, res) =>
-    res.render('createInnovations', { user: req.user });
+  res.render('createInnovations', { user: req.user });
 
-exports.innovationFile = async(req, res) => {
-    console.log('innovations file saved');
-    res.redirect(`/innovations?stage=ongoing`);
+exports.innovationFile = async (req, res) => {
+  console.log('innovations file saved');
+  res.redirect(`/innovations?stage=ongoing`);
 };
-exports.postInnovations = async(req, res) => {
-    if (!req.user) {
-        res.send('User not found');
-    }
-    const { validateInnovationData } = require('../validations/innovations');
-    const { error } = validateInnovationData(req.body);
-    if (error) {
-        console.log(error);
-        res.send({
-            status: false,
-            msg: 'error',
-        });
-    }
-    const newInn = new InnovationModel({
-        ...req.body,
-        description: makeSmallParagraphFromHTML([req.body], 'description')[0]
-            .description,
-        authorID: req.user._id,
-        isVerified: false,
-    });
-    console.log(newInn);
-    await newInn.save();
+exports.postInnovations = async (req, res) => {
+  if (!req.user) {
+    res.send('User not found');
+  }
+  const { validateInnovationData } = require('../validations/innovations');
+  const { error } = validateInnovationData(req.body);
+  if (error) {
+    console.log(error);
     res.send({
-        status: true,
-        msg: newInn._id,
+      status: false,
+      msg: 'error',
     });
+  }
+  const newInn = new InnovationModel({
+    ...req.body,
+    description: makeSmallParagraphFromHTML([req.body], 'description')[0]
+      .description,
+    authorID: req.user._id,
+    isVerified: false,
+  });
+  console.log(newInn);
+  await newInn.save();
+  res.send({
+    status: true,
+    msg: newInn._id,
+  });
 };
 
 const path = require('path');
-exports.downloadFile = async(req, res) => {
-    const filePath = path.join(
-        process.cwd(),
-        '/public',
-        '/innovation',
-        req.params.id
-    );
-    res.download(filePath);
+exports.downloadFile = async (req, res) => {
+  const filePath = path.join(
+    process.cwd(),
+    '/public',
+    '/innovation',
+    req.params.id
+  );
+  res.download(filePath);
 };
--- controllers/researches.js
+++ controllers/researches.js
@@ -4,135 +4,139 @@ const { makeSmallParagraphFromHTML } = require('./utils');
 
 const LIMIT = 9;
 
-exports.getResearch = async(req, res) => {
-    console.log(req.params.id);
-    const data = await ResearchModel.findById(req.params.id);
-    res.render('singleResearch', { data, user: req.user });
+exports.getResearch = async (req, res) => {
+  console.log(req.params.id);
+  const data = await ResearchModel.findById(req.params.id);
+  res.render('singleResearch', { data, user: req.user });
 };
 
-exports.getResearches = async(req, res) => {
-    const page = +req.query.page || 1;
-    const search = req.query.search;
-    let searchKey = { isVerified: true };
-    let baseUrl = req.baseUrl;
-    if (search) {
-        baseUrl += `?search=${search}&page=`;
-        const searchOption = {
-            $regex: search,
-            $options: 'i',
-        };
-        searchKey = {
-            $or: [
-                { title: searchOption },
-                { BriefDesciption: searchOption },
-                { conflictOfInterest: searchOption },
-                { financialSupport: searchOption },
-                { Acknowlegement: searchOption },
-                { references: searchOption },
-                { authors: searchOption },
-            ],
-        };
-    } else baseUrl += `?page=`;
+exports.getResearches = async (req, res) => {
+  const page = +req.query.page || 1;
+  const search = req.query.search;
+  let searchKey = { isVerified: true };
+  let baseUrl = req.baseUrl;
+  if (search) {
+    baseUrl += `?search=${search}&page=`;
+    const searchOption = {
+      $regex: search,
+      $options: 'i',
+    };
+    searchKey = {
+      $or: [
+        { title: searchOption },
+        { BriefDesciption: searchOption },
+        { conflictOfInterest: searchOption },
+        { financialSupport: searchOption },
+        { Acknowlegement: searchOption },
+        { references: searchOption },
+        { authors: searchOption },
+      ],
+    };
+  } else baseUrl += `?page=`;
 
-    const data = await ResearchModel.find(searchKey)
-        .limit(LIMIT)
-        .skip(LIMIT * (page - 1));
-    const totalItems = await ResearchModel.find(searchKey).countDocuments();
-    return res.render('researches', {
-        user: req.user,
-        data: makeSmallParagraphFromHTML(data, 'BriefDesciption'),
-        search,
-        ...pagination(page, LIMIT, totalItems, baseUrl),
+  const data = await ResearchModel.find(searchKey)
+    .limit(LIMIT)
+    .skip(LIMIT * (page - 1));
+  const totalItems = await ResearchModel.find(searchKey).countDocuments();
+  return res.render('researches', {
+    user: req.user,
+    data: makeSmallParagraphFromHTML(data, 'BriefDesciption'),
+    search,
+    ...pagination(page, LIMIT, totalItems, baseUrl),
+  });
+};
+
+exports.getAllResearches = async (req, res) => {
+  let { stage, searchInput, type } = req.query;
+  let data;
+  // console.log(searchInput);
+  if (searchInput) {
+    const keyword = {
+      researchStage: type,
+      isVerified: true,
+    };
+    searchInput = searchInput.trim();
+    let searchOptions = {
+      $regex: searchInput,
+      $options: 'i',
+    };
+    let searchKey = {
+      $or: [
+        { title: searchOptions },
+        { description: searchOptions },
+        { name: searchOptions },
+        { designation: searchOptions },
+        { email: searchOptions },
+        { phone: searchOptions },
+        { collaboration: searchOptions },
+        { collabScope: searchOptions },
+        { newsAndPub: searchOptions },
+      ],
+    };
+    data = await ResearchModel.find({
+      $and: [keyword, searchKey],
     });
-};
-
-exports.getAllResearches = async(req, res) => {
-    let { stage, searchInput, type } = req.query;
-    let data;
-    // console.log(searchInput);
-    if (searchInput) {
-        const keyword = {
-            researchStage: type,
-            isVerified: true,
-        };
-        searchInput = searchInput.trim();
-        let searchOptions = {
-            $regex: searchInput,
-            $options: 'i',
-        };
-        let searchKey = {
-            $or: [
-                { title: searchOptions },
-                { description: searchOptions },
-                { name: searchOptions },
-                { designation: searchOptions },
-                { email: searchOptions },
-                { phone: searchOptions },
-                { collaboration: searchOptions },
-                { collabScope: searchOptions },
-                { newsAndPub: searchOptions },
-            ],
-        };
-        data = await ResearchModel.find({
-            $and: [keyword, searchKey],
-        });
-        console.log(type);
-        if (type == 'ongoing') {
-            res.render('ongoingResearches', { data, search: null });
-        } else {
-            res.render('completeResearches', { data, search: null });
-        }
-    }
-    data = await ResearchModel.find({ researchStage: stage, isVerified: true });
-    if (stage == 'ongoing') {
-        res.render('ongoingResearches', { data, search: null });
+    console.log(type);
+    if (type == 'ongoing') {
+      res.render('ongoingResearches', { data, search: null, user: req.user });
     } else {
-        res.render('completeResearches', { data, search: null });
+      res.render('completeResearches', {
+        data,
+        search: null,
+        user: req.user,
+      });
     }
+  }
+  data = await ResearchModel.find({ researchStage: stage, isVerified: true });
+  if (stage == 'ongoing') {
+    res.render('ongoingResearches', { data, search: null, user: req.user });
+  } else {
+    res.render('completeResearches', { data, search: null, user: req.user });
+  }
 };
 
-exports.researchFile = async(req, res) => {
-    console.log('file saved');
-    res.redirect(`/researches?stage=${req.body.researchStage}`);
+exports.researchFile = async (req, res) => {
+  console.log('file saved');
+  res.redirect(`/researches?stage=${req.body.researchStage}`);
 };
-exports.postResearches = async(req, res) => {
-    if (!req.user) {
-        res.send('User not found');
-    }
-    const { validateResearchData } = require('../validations/researches');
-    const { error } = validateResearchData(req.body);
-    if (error) {
-        console.log(error);
-        res.send({
-            status: false,
-            msg: error.details[0].message,
-        });
-    }
-    const newResearch = new ResearchModel({
-        ...req.body,
-        description: makeSmallParagraphFromHTML([req.body], 'description')[0]
-            .description,
-        authorID: req.user._id,
-        isVerified: false,
-    });
-    console.log(newResearch);
-    await newResearch.save();
+exports.postResearches = async (req, res) => {
+  if (!req.user) {
+    res.send('User not found');
+  }
+  const { validateResearchData } = require('../validations/researches');
+  const { error } = validateResearchData(req.body);
+  if (error) {
+    console.log(error);
     res.send({
-        status: true,
-        msg: 'okay',
+      status: false,
+      msg: error.details[0].message,
     });
+  }
+  const newResearch = new ResearchModel({
+    ...req.body,
+    description: makeSmallParagraphFromHTML([req.body], 'description')[0]
+      .description,
+    authorID: req.user._id,
+    isVerified: false,
+  });
+  console.log(newResearch);
+  await newResearch.save();
+  res.send({
+    status: true,
+    msg: 'okay',
+  });
 };
 
 exports.getNewResearches = (req, res) =>
-    res.render('createResearches', { user: req.user });
+  res.render('createResearches', { user: req.user });
 
 const path = require('path');
-exports.downloadFile = async(req, res) => {
-    const filePath = path.join(
-        process.cwd(),
-        '/public',
-        '/research',
-        req.params.id
-    );
-    res.download(filePath);
+exports.downloadFile = async (req, res) => {
+  const filePath = path.join(
+    process.cwd(),
+    '/public',
+    '/research',
+    req.params.id
+  );
+  res.download(filePath);
 };

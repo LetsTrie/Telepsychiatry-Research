b1abcd90fdaf978d5e1bc9b1952f3456662c8363
letstrie <2016-314-436@student.cse.du.ac.bd>
2020-05-22 14:08:19 +0600

    Task Completed..

--- routes/services.js
+++ routes/services.js
@@ -1,62 +1,55 @@
 const router = require('express').Router();
 
 const {
-    bookAppointment,
-    consultation,
-    searchConsultation,
-    psychoTherapy,
-    searchPsychoTherapy,
-    singleDoctorConsultation,
-    getAllResearchers,
-    searchResearchers,
-    getBookAppointment,
-    getChamberTimes,
-    allAppointments,
-    dateTimeReset,
+  bookAppointment,
+  consultation,
+  searchConsultation,
+  psychoTherapy,
+  searchPsychoTherapy,
+  singleDoctorConsultation,
+  getAllResearchers,
+  searchResearchers,
+  getBookAppointment,
+  getChamberTimes,
+  allAppointments,
+  dateTimeReset,
 } = require('../controllers/services');
 
 router.get('/consultation', consultation);
 router.get('/consultation/search', searchConsultation);
-
 router.get('/psychoTherapy', psychoTherapy);
 router.get('/psychoTherapy/search', searchPsychoTherapy);
-// (req, res, next) =>
-//   res.render('psycho_therapy')
-// );
-
 router.get('/doctor/profile/:id', singleDoctorConsultation);
 
 router.get('/institution', (req, res, next) => {
-    res.render('institution', { user: req.user });
+  res.render('institution', { user: req.user });
 });
 
 router.get('/assessment', (req, res, next) => {
-    res.render('assessment', { user: req.user });
+  res.render('assessment', { user: req.user });
 });
 
 router.get('/assessment/result', (req, res, next) => {
-    res.render('assessment_result', { user: req.user });
+  res.render('assessment_result', { user: req.user });
 });
 router.get('/assessment/quiz', (req, res, next) => {
-    res.render('assessment_quiz', { user: req.user });
+  res.render('assessment_quiz', { user: req.user });
 });
 
 router.get('/assessment/register', (req, res, next) => {
-    res.render('assessment_reg', { user: req.user });
+  res.render('assessment_reg', { user: req.user });
 });
 
 router.get('/special_services', (req, res, next) => {
-    res.render('specialServices', { user: req.user });
+  res.render('specialServices', { user: req.user });
 });
 
 router.post('/book', bookAppointment);
-
 router.get('/researchers', getAllResearchers);
 router.get('/search/researchers', searchResearchers);
-
 router.get('/bookAppointment/:expID', getBookAppointment);
 router.get('/getTimes', getChamberTimes);
-
 router.get('/all-appointments', allAppointments);
 router.post('/date-time-reset', dateTimeReset);
+
 module.exports = router;

dafe89ce4e2053160ab3e1d204582accd3d7438c
Meheraj Hossain <meherajj@gmail.com>
2020-06-02 16:46:35 +0600

    specialServiceDetails partially done

--- routes/admin.js
+++ routes/admin.js
@@ -1,127 +1,130 @@
 const router = require('express').Router();
 
 const {
-    getLogin,
-    contactUs,
-    adminGetResearch,
-    getResearch,
-    approveResearch,
-    disapproveResearch,
-    getInnovations,
-    singleInnoavtion,
-    approveInnovation,
-    disapproveInnovation,
-    postAddDoctor,
-    replyEmail,
+  getLogin,
+  contactUs,
+  adminGetResearch,
+  getResearch,
+  approveResearch,
+  disapproveResearch,
+  getInnovations,
+  singleInnoavtion,
+  approveInnovation,
+  disapproveInnovation,
+  postAddDoctor,
+  replyEmail,
 
-    // tests
-    getAllTests,
-    newTest,
-    postLogin,
-    createTest,
-    getSingleTest,
-    updateTest,
-    postUpdateTest,
-    getDashboard,
-    findTestbyDisorder,
-    addTestVersion,
-    postTestVersion,
+  // tests
+  getAllTests,
+  newTest,
+  postLogin,
+  createTest,
+  getSingleTest,
+  updateTest,
+  postUpdateTest,
+  getDashboard,
+  findTestbyDisorder,
+  addTestVersion,
+  postTestVersion,
 
-    // researches
-    getAdminResearches,
-    getAdminResearch,
-    getAdminNewResearch,
-    postResearches,
-    researchFile,
-    getAllResearches,
-    getAdminUpdateResearch,
-    postAdminUpdateResearch,
-    getUnverifiedResearches,
+  // researches
+  getAdminResearches,
+  getAdminResearch,
+  getAdminNewResearch,
+  postResearches,
+  researchFile,
+  getAllResearches,
+  getAdminUpdateResearch,
+  postAdminUpdateResearch,
+  getUnverifiedResearches,
 
-    // innovations
-    postInnovation,
-    innovationFile,
-    getAdminInnovations,
-    getAdminInnovation,
-    getAdminUpdateInnovation,
-    postAdminUpdateInnovation,
-    getUnverifiedInnoations,
+  // innovations
+  postInnovation,
+  innovationFile,
+  getAdminInnovations,
+  getAdminInnovation,
+  getAdminUpdateInnovation,
+  postAdminUpdateInnovation,
+  getUnverifiedInnoations,
 
-    // workshops
-    postWorkshop,
-    workshopFile,
-    getWorkshop,
-    singleWorkshop,
-    getUpdateWorkshop,
-    postUpdateWorkshop,
+  // workshops
+  postWorkshop,
+  workshopFile,
+  getWorkshop,
+  singleWorkshop,
+  getUpdateWorkshop,
+  postUpdateWorkshop,
 
-    // backup
-    getBackup,
+  //services
+  getAdminNewSpecialService,
+
+  // backup
+  getBackup,
 } = require('../controllers/admin');
 
 const {
-    adminAccess,
-    canNotBeAuthenticated,
+  adminAccess,
+  canNotBeAuthenticated,
 } = require('../middlewares/authorization');
 
 const multer = require('multer');
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/research/');
-    },
-    filename: (req, file, cb) => {
-        const filename = req.body.filename;
-        cb(null, filename);
-    },
+  destination: (req, file, cb) => {
+    cb(null, 'public/research/');
+  },
+  filename: (req, file, cb) => {
+    const filename = req.body.filename;
+    cb(null, filename);
+  },
 });
 
 const innovationStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        console.log('here');
-        cb(null, 'public/innovation/');
-    },
-    filename: (req, file, cb) => {
-        const filename = req.body.filename;
-        cb(null, filename);
-    },
+  destination: (req, file, cb) => {
+    console.log('here');
+    cb(null, 'public/innovation/');
+  },
+  filename: (req, file, cb) => {
+    const filename = req.body.filename;
+    cb(null, filename);
+  },
 });
 
 const workshopStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        console.log('here');
-        cb(null, 'public/workshop/');
-    },
-    filename: (req, file, cb) => {
-        const filename = req.body.filename;
-        cb(null, filename);
-    },
+  destination: (req, file, cb) => {
+    console.log('here');
+    cb(null, 'public/workshop/');
+  },
+  filename: (req, file, cb) => {
+    const filename = req.body.filename;
+    cb(null, filename);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if (
-        file.mimetype === 'image/png' ||
-        file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg'
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 
 const uploadResearchFile = multer({
-    storage: fileStorage,
-    fileFilter: fileFilter,
+  storage: fileStorage,
+  fileFilter: fileFilter,
 }).single('researchFile');
 
 const uploadInnovationFile = multer({
-    storage: innovationStorage,
-    fileFilter: fileFilter,
+  storage: innovationStorage,
+  fileFilter: fileFilter,
 }).single('innovationFile');
 
 const uploadPhotoWorkshop = multer({
-    storage: workshopStorage,
-    fileFilter: fileFilter,
+  storage: workshopStorage,
+  fileFilter: fileFilter,
 }).single('workshopFile');
 
 // OKAY
@@ -130,7 +133,7 @@ router.get('/login', canNotBeAuthenticated, getLogin);
 router.post('/login', canNotBeAuthenticated, postLogin);
 router.get('/tests', adminAccess, getAllTests);
 router.get('/test/new', adminAccess, (req, res, next) =>
-    res.render('admin_test_new', newTest)
+  res.render('admin_test_new', newTest)
 );
 router.post('/test/new', adminAccess, createTest);
 router.get('/test/single/:id', adminAccess, getSingleTest);
@@ -139,8 +142,8 @@ router.post('/test/update', adminAccess, postUpdateTest);
 router.get('/test/version/:id', adminAccess, addTestVersion);
 router.post('/test/version', adminAccess, postTestVersion);
 router.get('/logout', adminAccess, (req, res, next) => {
-    req.logout();
-    res.redirect('/admin/login');
+  req.logout();
+  res.redirect('/admin/login');
 });
 router.post('/findTestbyDisorder', adminAccess, findTestbyDisorder);
 
@@ -150,7 +153,7 @@ router.post('/replyEmail', replyEmail);
 
 //admin Add Doctors
 router.get('/addDoctors', (req, res) => {
-    res.render('addDoctors', { user: req.user });
+  res.render('addDoctors', { user: req.user });
 });
 router.post('/postAddDoctor', postAddDoctor);
 
@@ -172,10 +175,10 @@ router.get('/research/:id', adminAccess, getAdminResearch);
 router.get('/new/research', adminAccess, getAdminNewResearch);
 router.post('/research/new', adminAccess, postResearches);
 router.post(
-    '/research/new/file',
-    adminAccess,
-    uploadResearchFile,
-    researchFile
+  '/research/new/file',
+  adminAccess,
+  uploadResearchFile,
+  researchFile
 );
 router.get('/research/update/:id', adminAccess, getAdminUpdateResearch);
 router.post('/research/update', adminAccess, postAdminUpdateResearch);
@@ -184,19 +187,20 @@ router.get('/researches/unverified', adminAccess, getUnverifiedResearches);
 // special services
 router.get('/new/specialService', adminAccess, getAdminNewSpecialService);
 router.post('/new/specialService', adminAccess, (req, res) => {
-    // form data database e rakhte hobe
-    return res.json(req.body);
+  // form data database e rakhte hobe
+  return res.json(req.body);
 });
 
 // admin innovations
 router.get('/innovation/new', adminAccess, (req, res) => {
-    const { user } = req.user;
-    return res.render('addInnovationFromAdmin', { user });
+  const { user } = req.user;
+  return res.render('addInnovationFromAdmin', { user });
 });
 router.post('/innovation/new', adminAccess, postInnovation);
 router.post(
-    '/innovation/new/file', [adminAccess, uploadInnovationFile],
-    innovationFile
+  '/innovation/new/file',
+  [adminAccess, uploadInnovationFile],
+  innovationFile
 );
 router.get('/innovations', adminAccess, getAdminInnovations);
 router.get('/innovation/:id', adminAccess, getAdminInnovation);
@@ -206,12 +210,13 @@ router.get('/innovations/unverified', adminAccess, getUnverifiedInnoations);
 
 // admin workshop
 router.get('/workshop/new', (req, res) => {
-    res.render('add-new-workshop');
+  res.render('add-new-workshop');
 });
 router.post('/workshop/new', postWorkshop);
 router.post(
-    '/workshop/new/file', [adminAccess, uploadPhotoWorkshop],
-    workshopFile
+  '/workshop/new/file',
+  [adminAccess, uploadPhotoWorkshop],
+  workshopFile
 );
 router.get('/workshop', adminAccess, getWorkshop);
 router.get('/workshop/:id', adminAccess, singleWorkshop);
